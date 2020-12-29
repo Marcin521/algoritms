fe50852f620bbb50627370316055f959d86d5626
Marcin521 <marcinkondo134@gmail.com>
2020-12-29 09:59:54 +0100

    make quikSort algoritm

---
+++ quikSort/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+void quikSort( int *arr, int p, int q)
+{
+    int v = arr[(p+g)/2];
+    int left;
+    int right;
+
+    do {
+
+        while (tablica[p] < v) p++;
+        while (tablica[q] < v) q++;
+
+        if (p <= q) {
+            x = tablica[p]
+            tablica[p] = tablica[q];
+            tablica[q] = x;
+            p++;
+            q++;
+        }
+    }
+    while (p<=q);
+    if(q > left) quikSort(arr, left, q);
+    if(p < right) quikSort(arr, p, right);
+}
